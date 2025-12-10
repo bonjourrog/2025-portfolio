0666ee7b65cfbf2ffc059344f7faaf68164e9d4b
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-12-10 12:02:15 -0700

    feat: add experience, link, project and skillsCategory entities

---
+++ entity/experience.ts
@@ -0,0 +1,8 @@
+export interface Experience{
+    id: number;
+    title: string;
+    role: string;
+    date: string;
+    description: string;
+    skills: string[];
+}
---
+++ entity/skillsCategory.ts
@@ -0,0 +1,5 @@
+export interface SkillCategory {
+    id: number;
+    category: string;
+    skills: string[];
+}
